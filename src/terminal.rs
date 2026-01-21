1e9339fdc6c7027f4543c342536584e58c39c6b2
Daniil <dadyarri@gmail.com>
2026-01-21 03:06:51 +0300

    reformat code

--- src/terminal.rs
+++ src/terminal.rs
@@ -5,31 +5,35 @@ use term;
 // Some examples on how to use the term crate
 
 pub fn error(message: &str) {
-    match term::stderr() { Some(mut t) => {
-        match t.fg(term::color::BRIGHT_RED) {
-            Ok(_) => {
-                write!(t, "{}", message).unwrap();
-                t.reset().unwrap();
-            },
-            Err(_) => writeln!(t, "{}", message).unwrap()
-        };
-    } _ => {
-        eprint!("{}", message);
-    }}
+    match term::stderr() {
+        Some(mut t) => {
+            match t.fg(term::color::BRIGHT_RED) {
+                Ok(_) => {
+                    write!(t, "{}", message).unwrap();
+                    t.reset().unwrap();
+                }
+                Err(_) => writeln!(t, "{}", message).unwrap(),
+            };
+        }
+        _ => {
+            eprint!("{}", message);
+        }
+    }
 }
-
 
 pub fn success(message: &str) {
-    match term::stdout() { Some(mut t) => {
-        match t.fg(term::color::GREEN) {
-            Ok(_) => {
-                write!(t, "{}", message).unwrap();
-                t.reset().unwrap();
-            },
-            Err(_) => writeln!(t, "{}", message).unwrap()
-        };
-    } _ => {
-        eprint!("{}", message);
-    }}
+    match term::stdout() {
+        Some(mut t) => {
+            match t.fg(term::color::GREEN) {
+                Ok(_) => {
+                    write!(t, "{}", message).unwrap();
+                    t.reset().unwrap();
+                }
+                Err(_) => writeln!(t, "{}", message).unwrap(),
+            };
+        }
+        _ => {
+            eprint!("{}", message);
+        }
+    }
 }
-
