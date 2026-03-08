3b64cac40f09a6edea08b36b2d77470b3ed1ce40
Daniil <dadyarri@gmail.com>
2026-03-09 01:31:48 +0300

    added cli autocomplete generation

---
+++ build.rs
@@ -0,0 +1,11 @@
+use clap::ValueEnum;
+use clap_complete::{generate_to, Shell};
+
+include!("src/cli.rs");
+
+fn main() {
+    let mut app = build_cli();
+    for &shell in Shell::value_variants() {
+        generate_to(shell, &mut app, "dari", "completions/").ok();
+    }
+}
